905e3356d65de76b091f29d265355533d8c6bf6c
Henrique Oliveira <ho.henrique@proton.me>
2024-07-08 15:12:35 +0200

    Added dwm-flexipatch

---
+++ dwm/patch/include.h
@@ -0,0 +1,24 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+#include "bar.h"
+
+#include "bar_dwmblocks.h"
+#include "bar_ltsymbol.h"
+#include "bar_status.h"
+#include "bar_status2d.h"
+#include "bar_statuscmd.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+#include "bar_systray.h"
+
+/* Other patches */
+#include "fakefullscreenclient.h"
+#include "swallow.h"
+#include "togglefullscreen.h"
+#include "vanitygaps.h"
+#include "warp.h"
+/* Layouts */
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
