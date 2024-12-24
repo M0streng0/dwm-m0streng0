41c15db3b8c9f0bfb14b496fe577690eb9bfdc6c
Henrique Oliveira <ho.henrique@proton.me>
2024-12-24 18:07:06 +0100

    Make everything POSIX compatible
    Because I changed from BASH to DASH

--- st/config.h
+++ st/config.h
@@ -21,7 +21,7 @@ static char *url_opener = "xdg-open";
  * 4: value of shell in /etc/passwd
  * 5: value of shell in config.h
  */
-static char *shell = "/usr/bin/bash";
+static char *shell = "/bin/sh";
 char *utmp = NULL;
 /* scroll program: to enable use a string like "scroll" */
 char *scroll = NULL;
